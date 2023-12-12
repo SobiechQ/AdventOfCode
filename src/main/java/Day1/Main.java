976c136d08aab51a496f8f8c59a30478b7a328c5
Mikołaj <mikolajmikolaj090302@gmail.com>
2023-12-12 21:32:47 +0100

    Finished day 1

--- src/main/java/Day1/Main.java
+++ src/main/java/Day1/Main.java
@@ -10,42 +10,49 @@ import java.util.concurrent.atomic.AtomicLong;
 public class Main {
     public static void main(String[] args) {
         try (var stream = new BufferedReader(new FileReader("src/main/java/Day1/input"))) {
-            List<String> lines = stream.lines().toList();
-            var changed = lines.stream()
-                    .map(s -> {
-                        StringBuilder sCopy = new StringBuilder();
-                        for (int i = 0; i < s.length(); i++) {
-                            sCopy.append(s.charAt(i));
-                            for (Number number : Number.values())
-                                sCopy = new StringBuilder(sCopy.toString().replaceAll(number.name().toLowerCase(), String.valueOf(number.getValue())));
-
-                        }
-
-
-                        return sCopy.toString();
-                    })
-                    .map(s -> s.replaceAll("([A-Za-z])", ""))
-//                    .map(s -> s.chars().collect(ArrayList<Integer>::new, ArrayList::add, ArrayList::addAll))
-//                    .map(s -> Integer.parseInt(Character.toString(s.getFirst()) + Character.toString(s.getLast())))
-                    .toList();
-            var changedEnd = changed.stream()
-//                    .map(s -> s.replaceAll("([A-Za-z])", ""))
-                    .map(s -> s.chars().collect(ArrayList<Integer>::new, ArrayList::add, ArrayList::addAll))
-                    .map(s -> Integer.parseInt(Character.toString(s.getFirst()) + Character.toString(s.getLast())))
-                    .toList();
-
-
-
-            for (int i = 0; i < changed.size(); i++) {
-                System.out.println("ORIGINAL [" + lines.get(i) + "], CHANGED [" + changed.get(i) + "]" + ", CHANGED END [" + changedEnd.get(i) + "] ");
-            }
-            AtomicLong abc = new AtomicLong();
-            changedEnd.stream().forEach((i)-> abc.addAndGet(i));
-            System.out.println(abc.get());
+            System.out.println(stream.lines()
+                    .map(Main::reduceTwoNumbers)
+                    .map(Main::replaceNames)
+                    .map(s -> s.replaceAll("[a-z]", ""))
+                    .mapToInt(Integer::parseInt)
+                    .sum());
         } catch (IOException e) {
             e.printStackTrace();
-            // [fiveseven4eightwolq], CHANGED [5748], CHANGED END [58]  powinno być 52
-            //wiecej niż 54970
         }
+    }
+    public static String reduceTwoNumbers(String s){
+        StringBuilder sb = new StringBuilder();
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+            String string = sb.toString();
+            output.append(s.charAt(i));
+            for (Number number : Number.values())
+                if (string.contains(number.name().toLowerCase()) || string.contains(String.valueOf(number.getValue()))) {
+                    i = s.length();
+                    break;
+                }
+        }
+        sb.setLength(0);
+        for (int i = s.length()-1; i >= 0 ; i--) {
+            sb.reverse();
+            sb.append(s.charAt(i));
+            sb.reverse();
+            String string = sb.toString();
+            for (Number number : Number.values()){
+                if (string.contains(number.name().toLowerCase()) || string.contains(String.valueOf(number.getValue()))) {
+                    i = 0;
+                    break;
+                }
+            }
+        }
+        output.append(sb);
+        return output.toString();
+    }
+
+    public static String replaceNames(String s){
+        for (Number number : Number.values())
+            s = s.replace(number.name().toLowerCase(), String.valueOf(number.getValue()));
+        return s;
     }
 }
